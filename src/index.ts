5509a98b3fde6a2b332e7db92a59ec9fcd2b7772
Alexey Gorbenko <a.v.gor@mail.ru>
2024-01-31 15:32:20 +0300

    feat: add info in header section

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import './assets/scss/style.scss';
 
 console.log('ok!');
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Hello, webpack';
-
-  return element;
-}
-
-document.body.appendChild(component());
