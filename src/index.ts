d6c31bef0e63a3f6c9dea3b150f21fea52f63998
Alexey Gorbenko <a.v.gor@mail.ru>
2024-02-06 16:38:09 +0300

    refactor: continue layout header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import './assets/scss/style.scss';
 
 console.log('ok!');
+
+// const desmondName = <HTMLDivElement>document.querySelector('.header__fname');
+// desmondName.classList.replace('header__fname_unactive', 'header__fname_active');
