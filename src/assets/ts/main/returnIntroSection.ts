71abf32c729cd1137d292994e48cd03ed5af1e3a
Alexey Gorbenko <a.v.gor@mail.ru>
2024-04-08 16:27:54 +0300

    refactor: start add info about myself

--- src/assets/ts/main/returnIntroSection.ts
+++ src/assets/ts/main/returnIntroSection.ts
@@ -14,19 +14,19 @@ export default function returnIntroSection() {
   const introTitle = returnElement({
     tag: 'h2',
     classes: ['article-block__title', 'title'],
-    textContent: 'Intro',
+    textContent: 'О себе',
   });
   const introSubtitle = returnElement({
     tag: 'p',
     classes: ['article-block__subtitle', 'text'],
-    textContent: 'What I am all about.',
+    textContent: 'Ключевые компетенции.',
   });
   const introDescription = returnElement({
     tag: 'div',
     classes: ['section__description', 'section__block'],
   });
   introDescription.innerHTML = `
-    <p class="article-block__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aaliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed <a href="#" class="article-block__link">pretium</a>, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Nulla vitae mauris non felis mollis faucibus.</p>
+    <p class="article-block__text">Программированием заинтересовался уже имея богатый опыт работы системным администратором. В 2020 году с красным дипломом окончил <a href="http://rgkript.ru/" class="article-block__link">«Колледж радиоэлектроники, информационных и промышленных технологий»</a>. Изучение фронтенд-направления в веб-разработке привело  </p>
     <p class="article-block__text">Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna. Fusce lacinia arcu et nulla. Nulla vitae mauris non felis mollis.</p>
     `;
   introHeader.append(introTitle, introSubtitle);
