bf7ec91524b46a1e5456917d16d0d00cee6bf6de
Alexey Gorbenko <a.v.gor@mail.ru>
2024-04-11 16:23:29 +0300

    feat: add info in Expertise section

--- src/assets/ts/main/returnExpertiseSection.ts
+++ src/assets/ts/main/returnExpertiseSection.ts
@@ -5,45 +5,45 @@ export default function returnExpertiseSection() {
   expertiseSection.innerHTML = `
     <section class="section__wrapper article-block">
       <div class="section__header section__block article-block__header">
-        <h2 class="article-block__title title">Expertise</h2>
-        <p class="article-block__subtitle text">Batman would be jealous.</p>
+        <h2 class="article-block__title title">Компетенции</h2>
+        <p class="article-block__subtitle text">Профессиональные знания.</p>
       </div>
       <div class="section__description section__block">
         <ol class="article-block__ol ord-list list">
           <li class="ord-list__item list__item">
             <article class="list__article">
-              <h3 class="ord-list__title title">Advanced CSS</h3>
-              <p class="ord-list__text text">Cras ornare tristique elit lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id.</p>
+              <h3 class="ord-list__title title">HTML/CSS</h3>
+              <p class="ord-list__text text">Валидная семантическая верстка в соответствии с требованиями HTML5. Верстка по PixelPerfect. Использование Flexbox, Grid. Препроцессор SCSS. Адаптивная и отзывчивая верстка mobile-first / desktop-first.</p>
           </article>
             </li>
           <li class="ord-list__item list__item">
             <article class="list__article ord-list__article">
-              <h3 class="ord-list__title title">Front-end design</h3>
-              <p class="ord-list__text text">Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna. Ut aliquam sollicitudin leo.</p>
+              <h3 class="ord-list__title title">JavaScript</h3>
+              <p class="ord-list__text text">ES6. Типы данных, функции, объекты, классы, методы. Прототипы, наследование. Обработка ошибок. Замыкания. Промисы: fetch, async/await. Работа с DOM, события, жизненный цикл страницы. Модули. Web storage.</p>
             </article>
             </li>
           <li class="ord-list__item list__item">
             <article class="list__article ord-list__article">
-              <h3 class="ord-list__title title">Ruby on rails</h3>
-              <p class="ord-list__text text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Vivamus vestibulum nulla cras ornare tristique elit nec ante.</p>
+              <h3 class="ord-list__title title">Typescript</h3>
+              <p class="ord-list__text text">Базовые типы. Типы, интерфейсы. Дженерики. Объединения / пересечения типов. Избегание типа 'any'.</p>
             </article>
           </li>
           <li class="ord-list__item list__item">
             <article class="list__article ord-list__article">
-              <h3 class="ord-list__title title">Object-oriented PHP</h3>
-              <p class="ord-list__text text">Morbi in sem quis dui placerat ornare. Ut aliquam sollicitudin leo. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras ante. Vivamus vestibulum nulla nec ante.</p>
+              <h3 class="ord-list__title title">Программное обеспечение</h3>
+              <p class="ord-list__text text">Visual Studio Code, Chrome DevTools, PixelPerfect, Figma, GIMP, Inkscape, Adobe Photoshop.</p>
             </article>
           </li>
           <li class="ord-list__item list__item">
             <article class="list__article ord-list__article">
-              <h3 class="ord-list__title title">Wordpress</h3>
-              <p class="ord-list__text text">Phasellus ultrices nulla quis nibh. Fusce lobortis lorem. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, sodales sit amet, nisi.</p>
+              <h3 class="ord-list__title title">Средства разработки</h3>
+              <p class="ord-list__text text">Webpack, Typescript, Eslint, Stylelint, Prettier, Husky, SASS</p>
             </article>
           </li>
           <li class="ord-list__item list__item">
             <article class="list__article ord-list__article">
-              <h3 class="ord-list__title title">Javascript / Jquery</h3>
-              <p class="ord-list__text text">Cras ornare tristique elit lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id.</p>
+              <h3 class="ord-list__title title">Работа в команде</h3>
+              <p class="ord-list__text text">Agile-методология: Scrum, Kanban (Trello, Asana, GitHub Desktop). </p>
             </article>
           </li>
         </ol>
