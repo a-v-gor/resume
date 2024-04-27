ea748f799e5a3cad1445aff0c463f13e4f153101
Alexey Gorbenko <a.v.gor@mail.ru>
2024-04-27 13:59:35 +0300

    refactor: refactor inner section code

--- src/assets/ts/common/returnSection.ts
+++ src/assets/ts/common/returnSection.ts
@@ -1,12 +1,14 @@
 import returnElement from './returnElement';
 
 interface Props {
-  tag: string;
+  tag?: string;
   classes: string[];
   wrapperTag: string;
   wrapperClasses: string[];
   headerClasses: string[];
   descriptionClasses: string[];
+  title?: string;
+  subitle?: string;
 }
 
 interface resultObj {
@@ -16,8 +18,14 @@ interface resultObj {
 }
 
 export default function returnSection(props: Props): resultObj {
+  let tag = '';
+  if (props.tag !== undefined) {
+    tag = props.tag;
+  } else {
+    tag = 'div';
+  }
   const section = returnElement({
-    tag: props.tag,
+    tag: tag,
     classes: props.classes,
   });
   const wrapper = returnElement({
@@ -32,6 +40,19 @@ export default function returnSection(props: Props): resultObj {
     tag: 'div',
     classes: props.descriptionClasses,
   });
+  if (props.title !== undefined && props.subitle !== undefined) {
+    const title = returnElement({
+      tag: 'h2',
+      classes: ['article-block__title', 'title'],
+      textContent: props.title,
+    });
+    const subtitle = returnElement({
+      tag: 'p',
+      classes: ['article-block__subtitle', 'text'],
+      textContent: props.subitle,
+    });
+    header.append(title, subtitle);
+  }
 
   wrapper.append(header, description);
   section.append(wrapper);
--- src/assets/ts/main/returnIntroSection.ts
+++ src/assets/ts/main/returnIntroSection.ts
@@ -1,38 +1,23 @@
-import returnElement from '../common/returnElement';
-import returnSectionOld from '../common/returnSection-old';
+import returnSection from '../common/returnSection';
 
 export default function returnIntroSection() {
-  const sectionIntro = returnSectionOld();
-  const introWrapper = returnElement({
-    tag: 'article',
-    classes: ['section__wrapper', 'article-block'],
+  const sectionObj = returnSection({
+    classes: ['section'],
+    wrapperTag: 'article',
+    wrapperClasses: ['section__wrapper', 'article-block'],
+    headerClasses: [
+      'section__header',
+      'section__block',
+      'article-block__header',
+    ],
+    descriptionClasses: ['section__description', 'section__block'],
+    title: 'О себе',
+    subitle: 'Ключевые компетенции.',
   });
-  const introHeader = returnElement({
-    tag: 'div',
-    classes: ['section__header', 'section__block', 'article-block__header'],
-  });
-  const introTitle = returnElement({
-    tag: 'h2',
-    classes: ['article-block__title', 'title'],
-    textContent: 'О себе',
-  });
-  const introSubtitle = returnElement({
-    tag: 'p',
-    classes: ['article-block__subtitle', 'text'],
-    textContent: 'Ключевые компетенции.',
-  });
-  const introDescription = returnElement({
-    tag: 'div',
-    classes: ['section__description', 'section__block'],
-  });
-  introDescription.innerHTML = `
+  sectionObj.description.innerHTML = `
     <p class="article-block__text">Начинающий front-end-разработчик. Хочу реализовать свой потенциал и приобретенные знания в продуктовой команде.</p>
     <p class="article-block__text">Fornt-end&nbsp;&ndash; относительно новая для меня сфера, куда я перехожу из смежной&nbsp;&ndash; системного администрирования. ИТ-бэкграунд и интерес к программированию позволили с легкостью получить красный диплом в <a class="article-block__link link" href="http://rgkript.ru/" target="_blank" class="article-block__link">РГКРиПТ</a> по специальности «техник-программист». Затем&nbsp;&ndash; курсы JS / Front-end от <a class="article-block__link link" href="https://rs.school/courses/" target="_blank" class="article-block__link">RSSchool</a>: <a class="article-block__link link" href="https://rs.school/courses/javascript-preschool" target="_blank" class="article-block__link">подготовительный</a> и <a class="article-block__link link" href="https://rs.school/courses/javascript-mentoring-program" target="_blank" class="article-block__link">основной</a>, где занял почетное 118-е&nbsp;место (к слову, всего на курс поступило около 8&nbsp;200&nbsp;человек, успешно закончили&nbsp;&ndash; 531).</p>
     <p class="article-block__text">Хорошее знание английского языка позволяет изучать документацию в оригинале и исправлять / уточнять / публиковать на русском языке: <a class="article-block__link link" href="https://developer.mozilla.org/ru/docs/Web/CSS/CSS_grid_layout/Basic_concepts_of_grid_layout" target="_blank" class="article-block__link">статья на сайте mdn</a> (<a class="article-block__link link" href="https://github.com/mdn/translated-content/commit/57534ec7878cb97d33fff3173ca9d138bad4384d#diff-4b597b83b51827bfb8d612bf02c061810c70614513cae30f25ae1cd99ff486ce" target="_blank" class="article-block__link">ссылка на коммит</a>), <a class="article-block__link link" href="https://habr.com/ru/articles/768304/" target="_blank" class="article-block__link">статья</a> об использовании CSS.</p>
     <p class="article-block__text">За время обучения в RSSchool зарекомендовал себя как ответственный исполнитель и хороший командный игрок: отзывы опубликованы на странице <a class="article-block__link link" href="https://app.rs.school/cv/fb197dd1-e338-4171-9396-e96774f4f200" target="_blank" class="article-block__link">резюме</a> RSSchool.</p>`;
-  introHeader.append(introTitle, introSubtitle);
-  introWrapper.append(introHeader, introDescription);
-  sectionIntro.append(introWrapper);
-
-  return sectionIntro;
+  return sectionObj.section;
 }
