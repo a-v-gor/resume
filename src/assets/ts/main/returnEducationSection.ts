c69bb6ef583c41f24ea5d6549c75b766e9f1291a
Alexey Gorbenko <a.v.gor@mail.ru>
2024-05-02 15:33:19 +0300

    refactor: refactor education section code

--- src/assets/ts/main/returnEducationSection.ts
+++ src/assets/ts/main/returnEducationSection.ts
@@ -1,70 +1,59 @@
-import returnSectionOld from '../common/returnSection-old';
+import returnElement from '../common/returnElement';
+import returnEvolutionItem from '../common/returnEvolutionItem';
+import returnSectionObject from '../common/returnSectionObject';
 
 export default function returnEducationSection() {
-  const educationSection = returnSectionOld();
-  educationSection.innerHTML = `
-    <section class="section__wrapper article-block education">
-      <div class="section__header section__block article-block__header education__header">
-        <h2 class="article-block__title title">Образование</h2>
-        <p class="article-block__subtitle text">Scio me nihil scire.</p>
-      </div>
-      <div class="section__description section__block education__description">
-        <ul class="article-block__ul unord-list list evolution-ul education__ul">
-          <li class="unord-list__item list__item evolution-ul__item education__item">
-            <article class="list__article article-li evolution-ul__article">
-              <div class="article-li__header">
-                <h3 class="article-li__title title">Rolling Scopes School</h3>
-                <p class="article-li__period">2023</p>
-                <p class="article-li__position">JS/Frontend Development Course.</p>
-                <p class="article-li__location">Дистанционно</p>
-              </div>
-              <div class="article-li__body">
-                <p class="article-li__text text">Git, GitHub, оформление коммитов и пулл реквестов. Адаптивная верстка. Core JS. Алгоритмические задачи. DOM, DOM Events, Browser API. Основы NodeJS. Webpack.</p>
-                <p class="article-li__text text">TypeScript. Разработка UI-компонентов. Техническое интервью. Разработка в команде.</p>
-              </div>
-            </article>
-          </li>
-          <li class="unord-list__item list__item evolution-ul__item education__item">
-            <article class="list__article article-li evolution-ul__article">
-              <div class="article-li__header">
-                <h3 class="article-li__title title">Rolling Scopes School</h3>
-                <p class="article-li__period">2022</p>
-                <p class="article-li__position">JS/Frontend-разработка. Подготовительный этап</p>
-                <p class="article-li__location">Дистанционно</p>
-              </div>
-              <div class="article-li__body">
-                <p class="article-li__text text">Знакомство с системой контроля версий Git. Chrome Dev Tools, VS Code, Terminal. Основы HTML. Основы CSS. Знакомство с JavaScript. DOM API. DOM Events. Алгоритмы и Структуры Данных.</p>
-              </div>
-            </article>
-          </li>
-          <li class="unord-list__item list__item evolution-ul__item education__item">
-            <article class="list__article article-li evolution-ul__article">
-              <div class="article-li__header">
-                <h3 class="article-li__title title">Ростовский-на-Дону колледж радиоэлектроники, информационных и промышленных технологий</h3>
-                <p class="article-li__period">2016 – 2020</p>
-                <p class="article-li__position">Программист</p>
-                <p class="article-li__location">Ростов-на-Дону</p>
-              </div>
-              <div class="article-li__body">
-                <p class="article-li__text text">Информационные технологии. Основы проектирования баз данных. Численные методы. Компьютерные сети. Менеджмент в профессиональной деятельности. Основы финансовой грамотности и предпринимательства. Компьютерная графика.</p>
-              </div>
-            </article>
-          </li>
-          <li class="unord-list__item list__item evolution-ul__item education__item">
-            <article class="list__article article-li evolution-ul__article">
-              <div class="article-li__header">
-                <h3 class="article-li__title title">Краснодарский многопрофильный институт дополнительного образования</h3>
-                <p class="article-li__period">2019</p>
-                <p class="article-li__position">Инженер-программист</p>
-                <p class="article-li__location">Дистанционно</p>
-              </div>
-              <div class="article-li__body">
-                <p class="article-li__text text">Курс профессиональной переподготовки «Инженер-программист». Нормативно-правовое регулирование и законодательная база в сфере информационных и компьютерных технологий. Базовый английский язык для технической документации. Введение в программирование. Разработка Web-приложений на базе HTML5, CSS3, JavaScript.</p>
-              </div>
-            </article>
-          </li>
-        </ul>
-      </div>
-    </section>`;
-  return educationSection;
+  const educationSectionObject = returnSectionObject({
+    wrapperClasses: ['article-block', 'education'],
+    headerClasses: ['article-block__header', 'education__header'],
+    descriptionClasses: ['education__description'],
+    title: 'Образование',
+    subitle: 'Scio me nihil scire.',
+  });
+  const list = returnElement({
+    tag: 'ul',
+    classes: [
+      'article-block__ul',
+      'unord-list',
+      'list',
+      'evolution-ul',
+      'education__ul',
+    ],
+  });
+  const rssMainItem = returnEvolutionItem(
+    'Rolling Scopes School',
+    '2023',
+    'JS/Frontend Development Course.',
+    'Дистанционно',
+    'Git, GitHub, оформление коммитов и пулл реквестов. Адаптивная верстка. Core JS. Алгоритмические задачи. DOM, DOM Events, Browser API. Основы NodeJS. Webpack. TypeScript. Разработка UI-компонентов. Техническое интервью. Разработка в команде.',
+    true
+  );
+  const rssPreItem = returnEvolutionItem(
+    'Rolling Scopes School',
+    '2022',
+    'JS/Frontend-разработка. Подготовительный этап.',
+    'Дистанционно',
+    'Знакомство с системой контроля версий Git. Chrome Dev Tools, VS Code, Terminal. Основы HTML. Основы CSS. Знакомство с JavaScript. DOM API. DOM Events. Алгоритмы и Структуры Данных.',
+    true
+  );
+  const rkriptItem = returnEvolutionItem(
+    'Ростовский-на-Дону колледж радиоэлектроники, информационных и промышленных технологий',
+    '2016 – 2020',
+    'Программист',
+    'Ростов-на-Дону',
+    'Информационные технологии. Основы проектирования баз данных. Численные методы. Компьютерные сети. Менеджмент в профессиональной деятельности. Основы финансовой грамотности и предпринимательства. Компьютерная графика.',
+    true
+  );
+  const kmidoItem = returnEvolutionItem(
+    'Краснодарский многопрофильный институт дополнительного образования',
+    '2019',
+    'Инженер-программист',
+    'Дистанционно',
+    'Курс профессиональной переподготовки. Нормативно-правовое регулирование и законодательная база в сфере информационных и компьютерных технологий. Базовый английский язык для технической документации. Введение в программирование. Разработка Web-приложений на базе HTML5, CSS3, JavaScript.',
+    true
+  );
+
+  list.append(rssMainItem, rssPreItem, rkriptItem, kmidoItem);
+  educationSectionObject.description.append(list);
+  return educationSectionObject.section;
 }
--- src/assets/ts/main/returnExperienceSection.ts
+++ src/assets/ts/main/returnExperienceSection.ts
@@ -1,66 +1,6 @@
 import returnElement from '../common/returnElement';
+import returnEvolutionItem from '../common/returnEvolutionItem';
 import returnSectionObject from '../common/returnSectionObject';
-
-function returnEvolutionItem(
-  title: string,
-  period: string,
-  position: string,
-  location: string,
-  text: string
-) {
-  const item = returnElement({
-    tag: 'li',
-    classes: [
-      'unord-list__item',
-      'list__item',
-      'evolution-ul__item',
-      'experience__item',
-    ],
-  });
-  const article = returnElement({
-    tag: 'article',
-    classes: ['list__article', 'article-li', 'evolution-ul__article'],
-  });
-  const header = returnElement({
-    tag: 'div',
-    classes: ['article-li__header'],
-  });
-  const articleTitle = returnElement({
-    tag: 'h3',
-    classes: ['article-li__title', 'title'],
-    textContent: title,
-  });
-  const articlePeriod = returnElement({
-    tag: 'p',
-    classes: ['article-li__period'],
-    textContent: period,
-  });
-  const articlePosition = returnElement({
-    tag: 'p',
-    classes: ['article-li__position'],
-    textContent: position,
-  });
-  const articleLocation = returnElement({
-    tag: 'p',
-    classes: ['article-li__location'],
-    textContent: location,
-  });
-  const body = returnElement({
-    tag: 'div',
-    classes: ['article-li__body'],
-  });
-  const articleText = returnElement({
-    tag: 'p',
-    classes: ['article-li__text', 'text'],
-    textContent: text,
-  });
-
-  header.append(articleTitle, articlePeriod, articlePosition, articleLocation);
-  body.append(articleText);
-  article.append(header, body);
-  item.append(article);
-  return item;
-}
 
 export default function returnExperienceSection() {
   const experienceSectionObject = returnSectionObject({
@@ -87,14 +27,16 @@ export default function returnExperienceSection() {
     '2021 – Настоящее время',
     'Front-end-разработчик',
     'Ростов-на-Дону',
-    'Применение на практике знаний, оттачивание навыков верстки. Применение новых стандартов, современных направлений в разработке веб-приложений.'
+    'Применение на практике знаний, оттачивание навыков верстки. Применение новых стандартов, современных направлений в разработке веб-приложений.',
+    false
   );
   const kmvItem = returnEvolutionItem(
     'ООО «КМВ-Сервис»',
     '2019 – Настоящее время',
     'Системный администратор',
     'Ростов-на-Дону',
-    'Техническое обслуживание компьютерной и оргтехники. Работа с заявками пользователей на первой и второй линии техподдержки. Устранение аппаратных и программных неполадок на автоматизированных рабочих местах. Несложный ремонт электроники.'
+    'Техническое обслуживание компьютерной и оргтехники. Работа с заявками пользователей на первой и второй линии техподдержки. Устранение аппаратных и программных неполадок на автоматизированных рабочих местах. Несложный ремонт электроники.',
+    false
   );
 
   list.append(petProjectsItem, kmvItem);
